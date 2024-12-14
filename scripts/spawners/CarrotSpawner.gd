f888e6adb7163d58e9c8de32f733b8624309d666
Odin Kammerloher <kammerloher.andreas@gmail.com>
2024-12-14 21:24:40 +0100

    Add maximum to spawned, untouched carrots to prevent infinite carrots

--- scripts/spawners/CarrotSpawner.gd
+++ scripts/spawners/CarrotSpawner.gd
@@ -4,10 +4,17 @@ var cargo_scene = load("res://scenes/Cargo.tscn")
 
 var rng = RandomNumberGenerator.new()
 
+var spawned_carrots := []
+var max_untouched_carrots := 50
+
 func _ready():
 	rng.randomize()
 
 func _on_SpawnTimer_timeout():
+	spawned_carrots = _get_untouched_spawned_carrots()
+	if spawned_carrots.size() >= max_untouched_carrots:
+		return
+
 	var x = rng.randf_range(-32, 32)
 	var y = rng.randf_range(-32, 32)
 	
@@ -17,5 +24,15 @@ func _on_SpawnTimer_timeout():
 	cargo._current_velocity = cargo.speed * Vector2.UP * 2
 	cargo.target = global_position + Vector2(x, y)
 	get_parent().add_child(cargo)
+	spawned_carrots.append(cargo)
 	
 	$SpawnTimer.wait_time = rng.randf_range(.1, 12)
+
+
+func _get_untouched_spawned_carrots():
+	var filtered_spawned_carrots := []
+	for carrot in spawned_carrots:
+		if carrot.status == carrot.STATUS.SPAWNING or carrot.status == carrot.STATUS.UNHARVESTED:
+			filtered_spawned_carrots.append(carrot)
+
+	return filtered_spawned_carrots
