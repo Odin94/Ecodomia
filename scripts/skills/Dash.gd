a2cb160481c6c182ba0884f27cb62b6a599de5fb
Odin Kammerloher <kammerloher.andreas@gmail.com>
2025-02-09 16:11:56 +0100

    Fix duplicate calls to "stop_dashing_after_timeout"

--- scripts/skills/Dash.gd
+++ scripts/skills/Dash.gd
@@ -29,19 +29,18 @@ func _init(parent: Node):
 
 func dash():
 	if not is_dashing:
+		is_dashing = true
 		current_dashes -= 1
 		recover_dash_after_timeout()
-		is_dashing = true
 		dash_timer.start()
-		stop_dashing_after_timeout()
 		
-		tween.stop_all() # Stop any existing tweens
+		tween.stop_all()
 		tween.interpolate_property(
 			self, "current_dash_speed", # Property to animate
 			max_dash_speed, min_dash_speed, # From max to min
 			dash_duration, # Time it takes
-			Tween.TRANS_QUAD, # Quadratic easing
-			Tween.EASE_OUT # Ease-out (fast start, slow end)
+			Tween.TRANS_QUAD,
+			Tween.EASE_OUT
 		)
 		tween.start()
 
@@ -59,6 +58,5 @@ func recover_dash_after_timeout():
 
 
 func stop_dashing_after_timeout():
-	yield (Engine.get_main_loop().create_timer(.4), "timeout")
 	is_dashing = false
 	current_dash_speed = max_dash_speed
